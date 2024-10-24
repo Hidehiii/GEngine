8f4ffd49c325179184a819c1d8a5ebdcde40fffc
Hidehi <117826481+Hidehiii@users.noreply.github.com>
2024-10-24 15:49:08 +0800

    持续改进script组件

--- GEngine/src/GEngine/Components/NativeScript/NativeScript.h
+++ GEngine/src/GEngine/Components/NativeScript/NativeScript.h
@@ -21,9 +21,30 @@ namespace GEngine
 			InstantiateFunc = [](){ return static_cast<ScriptableObject*>(new T()); };
 			DestroyFunc = [](NativeScript* ns) { delete ns->Instance; ns->Instance = nullptr; };
 		}
+
+		template<typename T>
+		void AddInstance()
+		{
+			m_Functions.emplace_back([]() { return static_cast<ScriptableObject*>(new T()); }, 
+									[](NativeScript* ns) { delete ns->Instance; ns->Instance = nullptr; });
+			m_Scripts.emplace_back(nullptr);
+		}
+
+		template<typename T>
+		void GetInstance()
+		{
+
+		}
+
+		template<typename T>
+		void RemoveInstance()
+		{
+
+		}
 	public:
 		ScriptableObject* Instance = nullptr;
-		std::vector<ScriptableObject*>		m_Scripts;
+		std::vector < std::pair<ScriptableObject* (*)(), void (*)(NativeScript*)>>		m_Functions;
+		std::vector<ScriptableObject*>													m_Scripts;
 	};
 }
 
