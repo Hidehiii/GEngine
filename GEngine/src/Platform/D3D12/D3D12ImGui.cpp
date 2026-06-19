0b09f83b3290d55d4ed9b315405e8c7cd3fb5717
Chen Yu <1175195719@qq.com>
2026-06-19 22:15:05 +0800

    准备写dx12 imgui

---
+++ GEngine/src/Platform/D3D12/D3D12ImGui.cpp
@@ -0,0 +1,11 @@
+#include "GEpch.h"
+#include "D3D12ImGui.h"
+#include "GEngine/Application.h"
+#include "ImGui/backends/imgui_impl_glfw.h"
+#include "ImGui/backends/imgui_impl_win32.h"
+#include "ImGui/backends/imgui_impl_dx12.h"
+
+namespace GEngine
+{
+
+}
---
+++ GEngine/src/Platform/D3D12/D3D12ImGui.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "GEngine/ImGui/PlatformImGui.h"
+
+namespace GEngine
+{
+	class GENGINE_API D3D12ImGui : public PlatformImGui
+	{
+	};
+}
+
+
